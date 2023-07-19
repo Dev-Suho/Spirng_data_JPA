0d6205d0751e38bb08fb555a12b3ffd600305386
Dev-Suho <devsuho99@gmail.com>
2023-07-19 17:22:13 +0900

    Persistable_0719

---
+++ data-jpa/src/test/java/spring/datajpa/repository/ItemRepositoryTest.java
@@ -0,0 +1,20 @@
+package spring.datajpa.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import spring.datajpa.entity.Item;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ItemRepositoryTest {
+
+    @Autowired ItemRepository itemRepository;
+
+    @Test
+    void save() {
+        Item item = new Item("A");
+        itemRepository.save(item);
+    }
+}
